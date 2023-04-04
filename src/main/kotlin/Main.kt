8a38ced8cc7d96ea65a9f277ae7c71b4190f53b1
Виктор Решетов <vityareshetov@yandex.com>
2023-04-04 20:26:49 +0300

    Обновил функции ввода вывода

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 private fun readStr() = readln() // string line
 
-private fun readInt() = readStr().toInt() // single int
+private fun readLong() = readStr().toLong() // single int
 
 private fun readDouble() = readStr().toDouble() // single double
 
 private fun readStrings() = readStr().split(" ") // list of strings
 
-private fun readInts() = readStrings().map { it.toInt() } // list of ints
+//private fun readInts() = readStrings().map { it.toInt() } // list of ints
 
 private fun readLongs() = readStrings().map { it.toLong() } // list of ints
 
